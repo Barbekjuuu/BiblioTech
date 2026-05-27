20d955ae3ca28e0d7f9667a8714b1b253aa18814
Barbekjuuu <aleksandra.chrzescjan@hotmail.com>
2026-05-27 14:11:35 +0200

    Improve admin panel - better image previews and upload for book covers

--- books/admin.py
+++ books/admin.py
@@ -1,52 +1,46 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Autor, Gatunek, Ksiazka, Egzemplarz, Rezerwacja
 
+class EgzemplarzInline(admin.TabularInline):
+    model = Egzemplarz
+    extra = 1
 
 @admin.register(Autor)
 class AutorAdmin(admin.ModelAdmin):
-    list_display = ('imie_nazwisko', 'zdjecie')
+    list_display = ('imie_nazwisko', 'zdjecie_preview')
     search_fields = ('imie_nazwisko',)
-    list_per_page = 20
 
+    def zdjecie_preview(self, obj):
+        if obj.zdjecie:
+            return format_html('<img src="{}" width="50" height="50" style="object-fit:cover;" />', obj.zdjecie.url)
+        return "Brak zdjęcia"
+    zdjecie_preview.short_description = "Zdjęcie"
 
 @admin.register(Gatunek)
 class GatunekAdmin(admin.ModelAdmin):
     list_display = ('nazwa',)
     search_fields = ('nazwa',)
 
-
-class EgzemplarzInline(admin.TabularInline):
-    model = Egzemplarz
-    extra = 1
-
-
 @admin.register(Ksiazka)
 class KsiazkaAdmin(admin.ModelAdmin):
-    list_display = ('tytul', 'autor', 'gatunek', 'data_wydania', 'liczba_egzemplarzy')
-    list_filter = ('gatunek', 'data_wydania')
-    search_fields = ('tytul', 'opis')
+    list_display = ('tytul', 'autor', 'gatunek', 'jezyk', 'data_wydania', 'okladka_preview')
+    list_filter = ('gatunek', 'jezyk', 'data_wydania')
+    search_fields = ('tytul', 'opis', 'autor__imie_nazwisko')
     inlines = [EgzemplarzInline]
 
-    def liczba_egzemplarzy(self, obj):
-        return obj.egzemplarze.count()
-    liczba_egzemplarzy.short_description = 'Liczba egzemplarzy'
-
+    def okladka_preview(self, obj):
+        if obj.okladka:
+            return format_html('<img src="{}" width="60" height="80" style="object-fit:cover;" />', obj.okladka.url)
+        return "Brak okładki"
+    okladka_preview.short_description = "Okładka"
 
 @admin.register(Egzemplarz)
 class EgzemplarzAdmin(admin.ModelAdmin):
     list_display = ('ksiazka', 'status')
     list_filter = ('status',)
-    search_fields = ('ksiazka__tytul',)
-
 
 @admin.register(Rezerwacja)
 class RezerwacjaAdmin(admin.ModelAdmin):
-    list_display = ('uzytkownik', 'egzemplarz', 'data_rezerwacji', 'data_waznosci', 'czy_wazna')
+    list_display = ('uzytkownik', 'egzemplarz', 'data_rezerwacji', 'data_waznosci')
     list_filter = ('data_rezerwacji',)
-    search_fields = ('uzytkownik__username', 'egzemplarz__ksiazka__tytul')
-    readonly_fields = ('data_rezerwacji',)
-
-    def czy_wazna(self, obj):
-        return obj.data_waznosci > timezone.now()
-    czy_wazna.boolean = True
-    czy_wazna.short_description = 'Czy ważna?'
